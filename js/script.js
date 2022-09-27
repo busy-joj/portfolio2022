2870fda64cbd0028c6ade1d44eab753141b15676
jyj <busy.joj@gmail.com>
2022-09-28 03:31:41 +0900

    main publising

--- js/script.js
+++ js/script.js
@@ -68,3 +68,38 @@ $(function () {
     $('#menu-page').toggleClass('overlay');
   });
 });
+
+$(function () {
+  $('body').on('mousemove', function (e) {
+    var posX = e.pageX / 70;
+    var posY = e.pageY / 70;
+
+    $('.obj1').css({ 'right': 50 - posX });
+    $('.obj2').css({ 'top': -120 + posY });
+    $('.obj3').css({ 'right': 150 + posX });
+
+    $('.obj4').css({ 'bottom': -70 - posY });
+    $('.obj5').css({ 'left': 0 + posX });
+
+    $('.obj6').css({ 'top': -100 - posY });
+    $('.obj7').css({ 'left': 174 - posX });
+
+    $('.obj8').css({ 'bottom': -160 + posY });
+    $('.obj9').css({ 'left': 400 + posX });
+    $('.obj10').css({ 'top': -155 + posY });
+  });
+
+  /* 메뉴 관련 부분 */
+  var menu_on = $('#menu_wrap #menu li');
+  var contents = $('#contents > div');
+  var btn = $('#floatdiv ul li');
+  menu_on.click(function (e) {
+    e.preventDefault();
+    var ht = $(window).height();
+    var i = $(this).index();
+    var section = contents.eq(i);
+    var nowTop = i * ht;
+    $('html, body').stop().animate({ scrollTop: nowTop }, 1500);
+  });
+});
+

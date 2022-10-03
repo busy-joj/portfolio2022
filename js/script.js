ef30c49b5a5bc186aa25336a481f3263710cf08e
jyj <busy.joj@gmail.com>
2022-10-03 18:58:31 +0900

    포트폴리오 1차 작업

--- js/script.js
+++ js/script.js
@@ -101,5 +101,22 @@ $(function () {
     var nowTop = i * ht;
     $('html, body').stop().animate({ scrollTop: nowTop }, 1500);
   });
+
+  var swiper = new Swiper(".mySwiper", {
+    spaceBetween: 30,
+    centeredSlides: true,
+    // autoplay: {
+    //   delay: 2500,
+    //   disableOnInteraction: false,
+    // },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
 });
 

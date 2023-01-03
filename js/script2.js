f1f6e4ddbeb85ff65c5b3f6d45e6532bec1d33a9
jyj <busy.joj@gmail.com>
2023-01-04 00:28:02 +0900

    슬라이드 작업

--- js/script2.js
+++ js/script2.js
@@ -1,3 +1,13 @@
 $(function () {
     $('.header').addClass('on')
+
+    var swiper = new Swiper(".mySwiper", {
+        slidesPerView: 2,
+        spaceBetween: 50,
+        freeMode: true,
+        pagination: {
+            el: ".swiper-pagination",
+            clickable: true,
+        },
+    });
 })

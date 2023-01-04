4d5a7420f58710495f4f3b605868c2b2d5501e7c
jyj <busy.joj@gmail.com>
2023-01-04 23:48:53 +0900

    header 작업

--- js/script2.js
+++ js/script2.js
@@ -1,5 +1,8 @@
 $(function () {
-    $('.header').addClass('on')
+    setTimeout(function () {
+        $('.header').addClass('on');
+        $('.page').delay(500).removeClass('no-scroll');
+    }, 6400);
 
     var swiper = new Swiper(".mySwiper", {
         slidesPerView: 2,

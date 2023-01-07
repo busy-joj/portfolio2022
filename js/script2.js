edb388550a9ec0e5da25f62841892082e84de7e5
jyj <busy.joj@gmail.com>
2023-01-07 18:31:01 +0900

    scroll Up&Down 작업

--- js/script2.js
+++ js/script2.js
@@ -22,4 +22,33 @@ $(function () {
             el: ".swiper-pagination",
         },
     });
+
+    // nav & mousewheel
+    var menu_on = $('.nav-list li');
+    var contents = $('.content > div');
+    var page = 1;
+
+    menu_on.click(function (e) {
+        e.preventDefault();
+        var ht = $(window).height();
+        var i = $(this).index();
+        var nowTop = i * ht;
+        menu_on.removeClass('active')
+        menu_on.eq(i).addClass('active')
+        $('.page').stop().animate({ scrollTop: nowTop }, 800);
+        page = i + 1
+    });
+
+    $(window).on("wheel", function (e) {
+        var ht = $('.section').innerHeight();
+        if (e.originalEvent.deltaY > 0 && page < contents.length) {
+            page++;
+        } else if (e.originalEvent.deltaY < 0 && page > 1) {
+            page--;
+        }
+        menu_on.eq(page - 1).trigger("click");
+        var posTop = (page - 1) * ht
+        $('.page').animate({ scrollTop: posTop }, 800);
+        console.log(page)
+    })
 })

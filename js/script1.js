174b3c57a00019be20eae516f71dba0765bcbac7
busy-joj <busy.joj@gmail.com>
2023-02-24 17:58:00 +0900

    about 추가

--- js/script1.js
+++ js/script1.js
@@ -72,6 +72,22 @@ $(function () {
             e.preventDefault();
         });
     });
-   
+
+    var swiper = new Swiper(".mySwiper", {
+        spaceBetween: 30,
+        centeredSlides: true,
+        // autoplay: {
+        //   delay: 2500,
+        //   disableOnInteraction: false,
+        // },
+        pagination: {
+            el: ".swiper-pagination",
+            clickable: true,
+        },
+        navigation: {
+            nextEl: ".swiper-button-next",
+            prevEl: ".swiper-button-prev",
+        },
+    });
 })
 
